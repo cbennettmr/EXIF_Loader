2b29366818321c425a38523524f2845f3776f4a0
chrisb <chris.bennett.gis@gmail.com>
2015-06-02 21:48:29 -0400

    Rename project.

--- EXIF_Loader/Properties/AssemblyInfo.cs
+++ EXIF_Loader/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EXIF_Reader")]
+[assembly: AssemblyTitle("EXIF_Loader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EXIF_Reader")]
+[assembly: AssemblyProduct("EXIF_Loader")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
